90cdee33088818f40720e7a43d71d01ac55cb32c
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-05-18 11:13:05 +0300

    Tehtävä 2.6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -6,19 +6,47 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const Person = (props) => {
+    return (
+      <div>
+        {props.name}
+      </div>
+    )
+  } 
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('Clicked', event.target)
+
+    const nameObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+      {persons.map(person =>
+        <Person key={person.name} name={person.name} />
+        )}
+      </div>
     </div>
   )
 
