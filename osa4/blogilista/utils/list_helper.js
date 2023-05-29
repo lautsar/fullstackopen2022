6732f404d8a38fc6fb40b90cbe1420d841b5b4f7
Sari Lautanala <sari.lautanala@helsinki.fi>
2023-05-29 22:12:10 +0300

    Tehtävä 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const total = blogs
+    .map(blog => blog.likes)
+    .reduce((accumulator, current) => accumulator + current, 0)
+
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
